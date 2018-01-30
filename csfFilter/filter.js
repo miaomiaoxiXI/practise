e3197316a4178d85a885443732be2df1eb8d42f4
ym <2550994854@qq.com>
2018-01-30 18:04:06 +0800

    真的最终版，以前还是太年年轻啊
    加入了获取下拉复选框的数据，并最数据进行过滤处理

--- csfFilter/filter.js
+++ csfFilter/filter.js
@@ -9,11 +9,25 @@ app.controller("myCtrl",function($scope){
   {"name":"Herry","gender":"男",  "num":"006"},
   {"name":"Keive","gender":"男",  "num":"007"}
 ];
-
+  $scope.list=angular.copy($scope.pers);
   $scope.data={
-      items:["男","女","老","少"],
-      submit:function(){
-         //点击OK的操作
+      selected:[2],
+      items:[{name:"男",id:"1"},{name:"女",id:"2"}],
+      submit:function(a){
+        //点击OK的操作
+       var str=[],item=[],newArray=[];
+       var item=angular.copy($scope.list);
+       for(var i=0,l=a.length;i<l;i++){
+           str+=a[i].name;
+       }
+       for(var i=0,l=item.length;i<l;i++){
+           if(str.indexOf(item[i].gender)>-1){
+                newArray.push(item[i]);
+           }
+           $scope.pers=newArray;
+       }
+
+         console.log(arguments);
          console.log("ok");
       },
       cancel:function(){
@@ -23,12 +37,14 @@ app.controller("myCtrl",function($scope){
 
   };
 
+
+
 });
 
-app.directive('myFilter',[function(){
+app.directive('myFilter',['$compile',function($compile){
     return{
         restrict:'EA',
-        template:"<i class='blue filter icon'></i>",
+        template:"<i class='blue filter icon link'></i>",
         replace:true,
         scope:{
             options:'='
@@ -36,46 +52,61 @@ app.directive('myFilter',[function(){
         link:function(scope,elm,attrs,ctrl){
          
          //拼接popup中显示的html
-            var list = scope.options.items,
+            var list = scope.options.items,sel=scope.options.selected;
              html="<div class='conts'>"+
                      "<div class='options'>"+
                         "<ul>";
          for(var i=0,len=list.length;i<len;i++){
-                   html+="<li ng-repeat='x in itms'>"+
-                            "<div class='field'>"+
+                   html+="<li>"+
+                           
                                 "<div class='ui checkbox'>"+
-                                    "<input type='checkbox' name='example'>"+
-                                    "<label>"+list[i]+"</label>"+
-                                "</div>"+
+                                    "<input type='checkbox' name='example' value="+list[i].id+">"+
+                                    "<label for="+list[i].id+">"+list[i].name+"</label>"+
+                            
                             "</div>"+
                         "</li>";
          }
          
                  html+="</ul>"+
                     "</div>"+
-                    "<hr>"+
-                    "<button class='ui small  button filter-no'>"+
-                    "Reset"+
+                    "<div class='btns'>"+
+                    "<button class='ui small  button' ng-click='clr()'>"+
+                    "Clear"+
                     "</button>"+
-                    "<button class='ui small button filter-yes'>"+
+                    "<button class='ui small button' ng-click='sub()'>"+
                     "OK"+
                     "</button>"+
+                    "</div>"+
                     "</div>"
       
-            //事件委托
-            $("body").on("click",".filter-yes",function(){
-                scope.options.submit();
-                $('.icon').popup('hide');
-            })
-
-            $("body").on("click",".filter-no",function(){
-                scope.options.cancel();
-                $('.icon').popup('hide');
-            })
-            
             //执行popup程序
-            $('.icon').popup({ hoverable: true,html:html});
-            // $('checkbox').checkbox();
+            $(elm).popup({ on:"click",html:html,className:{popup:'ui test popup'},onCreate:function(){
+                $compile($('.ui.test.popup'))(scope);
+                for(var i=0,l=sel.length;i<l;i++){
+                    $('.ui.test.popup .ui.checkbox input[value='+sel[i]+']').prop("checked",true);
+                };$(".ui.test.popup .ui.checkbox").checkbox();
+            },duration:0});
+
+
+            scope.sub=function(){
+                sel=[];
+                newA=[],lists={};
+                for(i=0,l=list.length;i<l;i++){
+                    lists[list[i].id]=list[i];
+                }
+                $(".ui.test.popup .ui.checkbox input:checked").each(function(){
+                    newA.push(lists[$(this).val()]);
+                    sel.push($(this).val());
+                });
+                scope.options.submit(newA);
+                $(elm).popup('hide');
+            }
+
+            scope.clr=function(){
+                $(".ui.test.popup .ui.checkbox input:checked").prop("checked",false);
+                scope.options.cancel();
+                $(elm).popup('hide');
+            }
 
         }
     }
